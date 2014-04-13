22963f73c4c595f76cab83308163698c32b3afa5
Fernando Serboncini <fserb@fserb.com.br>
2014-04-14 01:23:32 +0200

    ugl: Add score communication

---
+++ vault/ugl/Score.hx
@@ -0,0 +1,26 @@
+package vault.ugl;
+
+class Score {
+  public function new(score: Float, ?final = false) {
+    post(score, final);
+  }
+
+#if html5
+  function post(score: Float, ?final = false) {
+    try {
+      var f = untyped __js__("window.parent.uglScore");
+      f(score, final);
+    } catch(e: Dynamic) { }
+  }
+#end
+
+#if flash
+  function post(score: Float, ?final = false) {
+    try {
+      flash.external.ExternalInterface.call("window.uglScore", score, final);
+    } catch(e: Dynamic) { }
+  }
+#end
+
+}
+
