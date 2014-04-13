e3af6c5be7b388702574964c189500f1fa076e2e
Fernando Serboncini <fserb@fserb.com.br>
2014-04-13 13:21:19 +0200

    ugl.Timer: make it stackable, simplify interface

--- vault/ugl/Timer.hx
+++ vault/ugl/Timer.hx
@@ -3,50 +3,39 @@ package vault.ugl;
 class Timer extends Entity {
   var _every: Float;
   var _delay: Float;
-  var _func: Void->Void;
-  var running: Bool;
+  var _func: List<Void->Bool>;
   public function new() {
     super();
-    running = false;
     _every = 0.0;
     _delay = 0.0;
-    _func = function() {};
+    _func = new List<Void->Bool>();
   }
 
   public function every(v: Float): Timer { _every = v; return this; }
   public function delay(v: Float): Timer { _delay = v; return this; }
 
-  public function run(f: Void->Void): Timer {
-    _func = f;
-    start();
-    return this;
-  }
-
-  public function start(): Timer {
-    running = true;
-    ticks = 0;
-    return this;
-  }
-
-  public function stop(): Timer {
-    running = false;
+  public function run(f: Void->Bool): Timer {
+    _func.add(f);
     return this;
   }
 
   override public function update() {
-    if (!running) return;
-
     if (_delay > 0.0) {
-      _delay -= Game.time;
-      if (_delay <= 0) {
-        _func();
-        remove();
+      if (ticks >= _delay) {
+        ticks = 0;
+        _func.pop()();
       }
-    } else if (_every > 0.0) {
+    } else {
       if (ticks >= _every) {
         ticks -= _every;
-        _func();
+        var f = _func.first();
+        if (!f()) {
+          _func.pop();
+        }
       }
+    }
+    if (_func.length == 0) {
+      remove();
     }
   }
 }
