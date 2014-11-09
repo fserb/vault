338ab68827730fb575fa774e2c8b6d83f9e41276
Fernando Serboncini <fserb@fserb.com.br>
2014-11-09 11:56:57 -0500

    Allow sort of Group members

--- vault/left/Group.hx
+++ vault/left/Group.hx
@@ -29,6 +29,16 @@ class Group extends Object {
     }
   }
 
+  public function sort(key: Dynamic -> Float) {
+    members.sort(function(a, b) {
+      var ka = key(a);
+      var kb = key(b);
+      if (ka < kb) return -1;
+      if (ka > kb) return 1;
+      return 0;
+    });
+  }
+
   override public function render(vp: View) {
     for (m in members) {
       if (!m.dead && m.visible) {
