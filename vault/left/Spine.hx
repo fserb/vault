de5f3b735549721824715a29533723c49fc60072
Fernando Serboncini <fserb@fserb.com.br>
2014-11-16 03:06:36 -0500

    Working spine

--- vault/left/Spine.hx
+++ vault/left/Spine.hx
@@ -5,100 +5,93 @@ import flash.geom.Rectangle;
 import haxe.io.Path;
 import haxe.Json;
 import openfl.Assets;
+import spinehaxe.animation.AnimationState;
+import spinehaxe.animation.AnimationStateData;
+import spinehaxe.attachments.Attachment;
+import spinehaxe.attachments.AttachmentLoader;
+import spinehaxe.attachments.AttachmentType;
+import spinehaxe.attachments.RegionAttachment;
+import spinehaxe.Bone;
+import spinehaxe.Skeleton;
+import spinehaxe.SkeletonData;
+import spinehaxe.SkeletonJson;
+import spinehaxe.Skin;
 import vault.Point;
 import vault.Vec2;
 
-typedef Bone = {
-  var name: String;
-  var length: Float;
-  var parent: Bone;
-  var pos: Vec2;
-  var angle: Float;
-  var scale: Vec2;
-};
+class SpineRegionAttachment extends RegionAttachment {
+  public var image: Image = null;
+}
+
+class SpineAttachmentLoader implements AttachmentLoader {
+  var atlas: Map<String, Image>;
+  public function new(atlas: Map<String, Image>) {
+    this.atlas = atlas;
+  }
+
+  public function newAttachment (skin:Skin, type:AttachmentType, name:String):Attachment {
+    if (type != region) {
+      throw "don't know how to deal with attachment type: " + type;
+    }
+
+    var att = new SpineRegionAttachment(name);
+    att.image = atlas[name];
+
+    return att;
+  }
+}
 
 class Spine extends Sprite {
   var atlas: Map<String, Image>;
-  var bones: Array<Bone>;
-  var setup_bones: Array<Bone>;
-  var slots: Array<{name: String, bone: Bone, attachment: String}>;
-  var setup_slots: Array<{name: String, bone: Bone, attachment: String}>;
-  var skins: Map<String, Map<String, Map<String, {x: Float, y: Float, angle: Float, width: Int, height: Int}>>>;
-  var animations: Map<String, {
-    bones: Map<String, {
-      translate: Array<{time: Float, curve: Array<Float>, pos: Vec2}>,
-      scale: Array<{time: Float, curve: Array<Float>, scale: Vec2}>,
-      rotate: Array<{time: Float, curve: Array<Float>, angle: Float}>
-      }>,
-    events: Array<{time: Float, name: String, int: Int, float: Float, string: String}>,
-    draworder: Array<{time: Float, offsets: Array<{slot: String, offset: Int}>}>
-    }>;
+  var skeletonData: SkeletonData;
+  public var skeleton: Skeleton;
+  public var state: AnimationState;
 
-  public function new(basename: String) {
+  public function new(basename: String, data: SkeletonData = null) {
     super();
-    atlas = loadAtlas(basename + ".atlas");
-    loadSpine(basename + ".json");
+
+    if (data == null) {
+      atlas = loadAtlas(basename + ".atlas");
+      var json = new SkeletonJson(new SpineAttachmentLoader(atlas));
+      skeletonData = json.readSkeletonData(Assets.getText(basename + ".json"), basename);
+    } else {
+      skeletonData = data;
+    }
+    skeleton = new Skeleton(skeletonData);
+    state = new AnimationState(new AnimationStateData(skeletonData));
   }
 
-  function loadSpine(filename: String) {
-    var spine = Json.parse(Assets.getText(filename));
-    bones = [];
-    setup_bones = [];
-    slots = [];
-    setup_slots = [];
-    skins = new Map();
-    animations = new Map();
-
-    var array: Array<Dynamic> = Reflect.field(spine, "bones");
-    var get = function(d: Dynamic, f: String, v: Dynamic): Dynamic {
-      if (!Reflect.hasField(d, f)) return v;
-      return Reflect.field(d, f);
-    }
-    for (bd in array) {
-      var b: Bone = { name: Reflect.field(bd, "name"),
-                      pos: new Vec2(Reflect.field(bd, "x"), Reflect.field(bd, "y")),
-                      length: get(bd, "length", 0.0),
-                      parent: null,
-                      angle: get(bd, "rotation", 0.0)*Math.PI/180,
-                      scale: new Vec2(1.0, 1.0) };
-      var parent = get(bd, "parent", null);
-      if (parent != null) {
-        b.parent = setup_bones.filter(function (x: Bone) return x.name == parent)[0];
-      }
-      setup_bones.push(b);
-      bones.push(Reflect.copy(b));
-    }
-
-    array = Reflect.field(spine, "slots");
-    for (sd in array) {
-      var b = Reflect.field(sd, "bone");
-      var sl = {name: Reflect.field(sd, "name"),
-                bone: setup_bones.filter(function (x: Bone) return x.name == b)[0],
-                attachment: Reflect.field(sd, "attachment")}
-      setup_slots.push(sl);
-      slots.push(Reflect.copy(sl));
-    }
-
-    var sks = Reflect.field(spine, "skins");
-    for (name in Reflect.fields(sks)) {
-      skins[name] = new Map();
-      for (slotname in Reflect.fields(Reflect.field(sks, name))) {
-        skins[name][slotname] = new Map();
-        for (attname in Reflect.fields(Reflect.field(Reflect.field(sks, name), slotname))) {
-          var f = Reflect.field(Reflect.field(Reflect.field(sks, name), slotname), attname);
-          skins[name][slotname][attname] = { x: Reflect.field(f, "x"),
-                                             y: Reflect.field(f, "y"),
-                                             angle: Reflect.field(f, "rotation")*Math.PI/180,
-                                             width: Reflect.field(f, "width"),
-                                             height: Reflect.field(f, "height") };
-        }
-      }
-    }
-    trace(skins);
-
+  override public function update() {
+    state.update(Left.elapsed);
+    state.apply(skeleton);
+    skeleton.updateWorldTransform();
+    skeleton.update(Left.elapsed);
   }
 
   override public function render(view: View) {
+    for (slot in skeleton.drawOrder) {
+      var att:SpineRegionAttachment = cast slot.attachment;
+      var bone:Bone = slot.bone;
+
+      var x = bone.worldX*0.05 + 0.05*att.x * bone.m00 + 0.05*att.y * bone.m01;
+      var y = (bone.worldY*0.05 + 0.05*att.x * bone.m10 + 0.05*att.y * bone.m11);
+
+      var angle = bone.worldRotation + att.rotation;
+
+      var scaleX = bone.worldScaleX + att.scaleX - 1;
+      var scaleY = bone.worldScaleY + att.scaleY - 1;
+
+      if (bone.worldFlipX) {
+        scaleX = -scaleX;
+        angle = -angle;
+      }
+      if (bone.worldFlipY) {
+        scaleY = -scaleY;
+        angle = -angle;
+      }
+
+      view.draw(att.image, pos.x + x, pos.y - y, angle*Math.PI/180, scaleX, scaleY, 1.0);
+    }
   }
 
   override function getImage(): Image {
@@ -135,7 +128,9 @@ class Spine extends Sprite {
           if (xy != null && size != null) {
             var b = new BitmapData(size.x, size.y, true, 0);
             b.copyPixels(bmp, new Rectangle(xy.x, xy.y, size.x, size.y), new flash.geom.Point(0, 0));
-            ret[name] = Image.create(b);
+            var im = Image.create(b);
+            // im.offset.x = im.offset.y = 0;
+            ret[name] = im;
           }
         }
       }
