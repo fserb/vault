18f18a781dbe4d2911de01579edd65806432bc77
Fernando Serboncini <fserb@fserb.com.br>
2015-01-02 12:19:26 -0500

    Left Text support

---
+++ vault/left/Text.hx
@@ -0,0 +1,96 @@
+package vault.left;
+
+import flash.display.BitmapData;
+import flash.geom.Matrix;
+import flash.geom.Point;
+import flash.geom.Rectangle;
+import openfl.Assets;
+import openfl.display.Tilesheet;
+import vault.BMFont;
+import vault.EMath;
+import vault.Vec2;
+
+class Char extends BMFont.BMChar {
+  public var image: Image;
+}
+
+enum TextAlign {
+  LEFT;
+  CENTER;
+  RIGHT;
+}
+
+enum TextVAlign {
+  TOP;
+  MIDDLE;
+  BOTTOM;
+}
+
+class Text extends Object {
+  public var pos: Vec2;
+  public var text: String;
+  public var xoffset: Float;
+  public var align: TextAlign;
+  public var valign: TextVAlign;
+
+  static var repo_font: Map<String, BMFont>;
+
+  var font: BMFont;
+
+  public function new(fontname: String) {
+    pos = Vec2.make(0, 0);
+    xoffset = 0.0;
+    align = LEFT;
+    valign = TOP;
+
+    if (repo_font == null) {
+      repo_font = new Map<String, BMFont>();
+    }
+
+    if (!repo_font.exists(fontname)) {
+      font = BMFont.read(Assets.getText("assets/fonts/" + fontname + ".fnt"));
+      var bmd = Assets.getBitmapData("assets/fonts/" + fontname + ".png");
+      repo_font.set(fontname, font);
+      for (id in font.chars.keys()) {
+        var c = new Char(font.chars[id]);
+        var b = new BitmapData(c.width, c.height);
+        b.copyPixels(bmd, new Rectangle(c.x, c.y, c.width, c.height), new Point(0, 0));
+        c.image = Image.create(b);
+        c.image.offset.x = -c.xoffset;
+        c.image.offset.y = -c.yoffset;
+        font.chars[id] = c;
+      }
+    } else {
+      font = repo_font.get(fontname);
+    }
+  }
+
+  override public function render(view: View) {
+    var dx = 0.0;
+    for (i in 0...text.length) {
+      var c: Char = cast font.chars[text.charCodeAt(i)];
+      dx += c.xadvance + xoffset;
+    }
+
+    var x = pos.x;
+    var y = pos.y;
+
+    switch (align) {
+      case LEFT:
+      case CENTER: x -= dx/2;
+      case RIGHT: x -= dx;
+    }
+
+    switch (valign) {
+      case TOP:
+      case MIDDLE: y -= font.lineHeight/2;
+      case BOTTOM: y -= font.lineHeight;
+    }
+
+    for (i in 0...text.length) {
+      var c: Char = cast font.chars[text.charCodeAt(i)];
+      view.draw(c.image, x, y);
+      x += c.xadvance + xoffset;
+    }
+  }
+}
