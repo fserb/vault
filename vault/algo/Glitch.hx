621c3d38481be3d4fd761e95575e0d985d40d555
Fernando Serboncini <fserb@fserb.com.br>
2015-02-11 11:41:28 -0500

    Faster alpha cleanup on Glitch

--- vault/algo/Glitch.hx
+++ vault/algo/Glitch.hx
@@ -3,49 +3,45 @@ package vault.algo;
 import flash.display.BitmapData;
 import flash.display.JPEGEncoderOptions;
 import flash.utils.ByteArray;
+import openfl.display.BitmapDataChannel;
 
 
 class Glitch {
-  static var addr = 0;
-  static public function glitch(source: BitmapData, iterations: Int, quality: Int = 50, alpha: Bool = true): BitmapData {
+  static var failedglitch = 0;
+
+  static public function glitch(source: BitmapData, quality: Int = 50, alpha: Bool = true): BitmapData {
     var jpg: ByteArray = source.encode("jpg", quality);
     // var header_length = getJPEGHeaderLength(jpg);
     var header_length = 611;
-    addr = iterations;
-    for (i in 0...iterations) {
-      glitchJPEGBytes(jpg, header_length);
-    }
-    var bmp = BitmapData.loadFromBytes(jpg);
-    if (bmp == null || bmp.width == 0 || bmp.height == 0) {
-      return glitch(source, iterations, quality);
+
+    var bmp: BitmapData = null;
+    while(true) {
+      var index = header_length + Std.int((jpg.length/2 - header_length - 5)*Math.random());
+      index = vault.EMath.min(jpg.length - 5, index);
+      jpg.position = index;
+      var b = jpg.readByte();
+      jpg.position = index;
+      jpg.writeByte(Std.int(Math.random()*256));
+
+      bmp = BitmapData.loadFromBytes(jpg);
+      if (bmp == null || bmp.width == 0 || bmp.height == 0) {
+        failedglitch++;
+        vault.left.Left.console.one("Glitch.failed", failedglitch);
+        jpg.position = index;
+        jpg.writeByte(b);
+      } else {
+        break;
+      }
     }
 
     if (alpha) {
-      var out = new BitmapData(source.width, source.height, true, 0);
-      var src = source.getPixels(source.rect);
-      var bm = bmp.getPixels(bmp.rect);
-      var o = new ByteArray();
-      src.position = bm.position = 0;
-      while (src.position < src.length) {
-        var s = src.readUnsignedInt();
-        var b = bm.readUnsignedInt();
-        o.writeUnsignedInt((s & 0xFF000000) | (b & 0xFFFFFF));
-      }
-      o.position = 0;
-      out.setPixels(source.rect, o);
-      trace("img: ", bmp.width, bmp.height);
+      var out = new BitmapData(bmp.width, bmp.height, true, 0);
+      out.copyPixels(bmp, bmp.rect, new flash.geom.Point(0, 0));
+      out.copyChannel(source, source.rect, new flash.geom.Point(0,0), BitmapDataChannel.ALPHA, BitmapDataChannel.ALPHA);
       return out;
     } else {
       return bmp;
     }
-  }
-
-  static function glitchJPEGBytes(jpg: ByteArray, header: Int) {
-    var index = header + Std.int((jpg.length - header - 5)*Math.random());
-    index = vault.EMath.min(jpg.length - 5, index);
-    trace(header, index, jpg.length);
-    jpg.position = index;
-    jpg.writeByte(Std.int(Math.random()*256));
   }
 
   static function getJPEGHeaderLength(jpg: ByteArray): Int {
