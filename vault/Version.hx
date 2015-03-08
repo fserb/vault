205befbbc952c096f6ba73a04a58f5c59400d414
Fernando Serboncini <fserb@fserb.com.br>
2015-03-08 18:06:49 -0400

    Use Version.getLine that always works

--- vault/Version.hx
+++ vault/Version.hx
@@ -9,14 +9,10 @@ class Version {
   }
 
   static function getLine(cmd: String, args: Array<String>): String {
-  #if (!linux && !windows)
-    return new Process(cmd, args).stdout.readLine();
-  #else
     Sys.command(cmd + " " + args.join(" ") + " > .tmp.vault.version");
     var f = sys.io.File.getContent(".tmp.vault.version");
     Sys.command("rm -f .tmp.vault.version");
     return StringTools.trim(f);
-  #end
   }
 
   macro static function getGitVersion() {
