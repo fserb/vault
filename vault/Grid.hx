5b5df3efa66d85a610a9f13326bedcdf89a239a1
Fernando Serboncini <fserb@fserb.com.br>
2015-10-09 02:03:19 -0400

    Support for offset grid

--- vault/Grid.hx
+++ vault/Grid.hx
@@ -7,6 +7,9 @@ import vault.Vec2;
 /*
 Grid is a 2D platform collision class
 
+TODO:
+- support for raycasting-view
+
 */
 
 typedef Rect = {
@@ -29,6 +32,7 @@ typedef Object = {
 }
 
 class Grid {
+  public var offset: Vec2;
   public var map: Array<Array<Tile>>;
   public var tilesize: Int;
   public var width: Int;
@@ -41,6 +45,7 @@ class Grid {
     this.height = height;
     this.tilesize = tilesize;
     this.debugsprite = debugsprite;
+    offset = new Vec2(0, 0);
     map = [];
     for (x in 0...width) {
       var c = new Array<Tile>();
@@ -79,10 +84,15 @@ class Grid {
   }
 
   function hit(rect: Rect, pos: Vec2): Bool {
-    var x1 = Std.int((pos.x + rect.x)/tilesize);
-    var y1 = Std.int((pos.y + rect.y)/tilesize);
-    var x2 = Std.int((pos.x + rect.x + rect.w - 1)/tilesize);
-    var y2 = Std.int((pos.y + rect.y + rect.h - 1)/tilesize);
+    var x1 = Std.int((pos.x + rect.x - offset.x)/tilesize);
+    var y1 = Std.int((pos.y + rect.y - offset.y)/tilesize);
+    var x2 = Std.int((pos.x + rect.x + rect.w - 1 - offset.x)/tilesize);
+    var y2 = Std.int((pos.y + rect.y + rect.h - 1 - offset.y)/tilesize);
+
+    if (x1 < 0 || y1 < 0 || x2 >= map.length || y2 >= map[0].length) {
+      return false;
+    }
+
     for (xx in x1...x2+1) {
       for (yy in y1...y2+1) {
         if (map[xx][yy] != null && map[xx][yy].block) {
@@ -98,6 +108,7 @@ class Grid {
     var pos = obj.pos.copy();
     pos.x += dx;
     pos.y += dy;
+
     if (hit(obj.rect, pos)) {
       obj.pos.x += dx;
       obj.pos.y += dy;
@@ -109,23 +120,31 @@ class Grid {
     if (dx < 0) obj.collide |= 8;
 
     if (dx > 0) {
-      var p = obj.pos.x + obj.rect.x + obj.rect.w;
+      var p = obj.pos.x + obj.rect.x + obj.rect.w - offset.x;
       var go = Math.floor((p + dx)/tilesize)*tilesize;
       tryDelta(obj, go - p, 0);
     } else if (dx < 0) {
-      var p = obj.pos.x + obj.rect.x;
+      var p = obj.pos.x + obj.rect.x - offset.x;
       var go = Math.ceil((p + dx)/tilesize)*tilesize;
       tryDelta(obj, go - p, 0);
     } else if (dy < 0) {
-      var p = obj.pos.y + obj.rect.y;
+      var p = obj.pos.y + obj.rect.y - offset.y;
       var go = Math.ceil((p + dy)/tilesize)*tilesize;
       tryDelta(obj, 0, go - p);
     } else if (dy > 0) {
-      var p = obj.pos.y + obj.rect.y + obj.rect.h;
+      var p = obj.pos.y + obj.rect.y + obj.rect.h - offset.y;
       var go = Math.floor((p + dy)/tilesize)*tilesize;
       tryDelta(obj, 0, go - p);
     }
     return false;
+  }
+
+  public function updateOffset(delta: Vec2) {
+    var d = delta.distance(offset);
+    offset = delta.copy();
+    for (k in objects) {
+      k.pos.add(d);
+    }
   }
 
   public function update(o: Dynamic, p: Vec2): Vec2 {
@@ -153,33 +172,36 @@ class Grid {
     obj.touch = 0;
 
     var sp = 1;
-    var left = Std.int((obj.pos.x + obj.rect.x-sp)/tilesize);
-    var up = Std.int((obj.pos.y + obj.rect.y-sp)/tilesize);
-    var right = Std.int((obj.pos.x + obj.rect.x + obj.rect.w+sp)/tilesize);
-    var down = Std.int((obj.pos.y + obj.rect.y + obj.rect.h+sp)/tilesize);
+    var left = Std.int((obj.pos.x + obj.rect.x-sp - offset.x)/tilesize);
+    var up = Std.int((obj.pos.y + obj.rect.y-sp - offset.y)/tilesize);
+    var right = Std.int((obj.pos.x + obj.rect.x + obj.rect.w+sp - offset.x)/tilesize);
+    var down = Std.int((obj.pos.y + obj.rect.y + obj.rect.h+sp - offset.y)/tilesize);
 
-    var x1 = Std.int((obj.pos.x + obj.rect.x)/tilesize);
-    var y1 = Std.int((obj.pos.y + obj.rect.y)/tilesize);
-    var x2 = Std.int((obj.pos.x + obj.rect.x + obj.rect.w-1)/tilesize);
-    var y2 = Std.int((obj.pos.y + obj.rect.y + obj.rect.h-1)/tilesize);
+    var x1 = Std.int((obj.pos.x + obj.rect.x - offset.x)/tilesize);
+    var y1 = Std.int((obj.pos.y + obj.rect.y - offset.y)/tilesize);
+    var x2 = Std.int((obj.pos.x + obj.rect.x + obj.rect.w - 1 - offset.x)/tilesize);
+    var y2 = Std.int((obj.pos.y + obj.rect.y + obj.rect.h - 1 - offset.y)/tilesize);
 
     for (xx in x1...x2+1) {
-      if (map[xx][up] != null && map[xx][up].block) obj.touch |= 1;
-      if (map[xx][down] != null && map[xx][down].block) obj.touch |= 4;
+      if (up < 0 || up >= map[0].length || xx < 0 || xx >= map.length ||
+          (map[xx][up] != null && map[xx][up].block)) obj.touch |= 1;
+      if (down < 0 || down >= map[0].length || xx < 0 || xx >= map.length ||
+          (map[xx][down] != null && map[xx][down].block)) obj.touch |= 4;
     }
     for (yy in y1...y2+1) {
-      if (map[right][yy] != null && map[right][yy].block) obj.touch |= 2;
-      if (map[left][yy] != null && map[left][yy].block) obj.touch |= 8;
+      if (right < 0 || right >= map.length || yy < 0 || yy >= map[0].length ||
+          (map[right][yy] != null && map[right][yy].block)) obj.touch |= 2;
+      if (left < 0 || left >= map.length || yy < 0 || yy >= map[0].length ||
+          (map[left][yy] != null && map[left][yy].block)) obj.touch |= 8;
     }
 
     return obj.pos.copy();
   }
 
   public function debug() {
+    return;
     var gfx = debugsprite.graphics;
     gfx.clear();
-
-    return;
     for (k in objects) {
       gfx.lineStyle(1, 0xFF0000, 0.2);
       gfx.drawRect(k.pos.x + k.rect.x, k.pos.y + k.rect.y, k.rect.w, k.rect.h);
